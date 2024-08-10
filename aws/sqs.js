228d6d30694762643942c15bfe541c17026bce38
sdornel <kai_dornel@hotmail.com>
2024-08-10 18:29:53 -0400

    build error

--- aws/sqs.js
+++ aws/sqs.js
@@ -1,14 +1,14 @@
-// this doesn't work yet. i am running into an issue with security credentials being incorrect
-const { SQSClient, SendMessageCommand, ReceiveMessageCommand } = require("@aws-sdk/client-sqs");
+// // this doesn't work yet. i am running into an issue with security credentials being incorrect
+// const { SQSClient, SendMessageCommand, ReceiveMessageCommand } = require("@aws-sdk/client-sqs");
 
-const region = process.env.AWS_REGION;
-// export const queueUrl = process.env.AWS_QUEUE_URL;
+// const region = process.env.AWS_REGION;
+// // export const queueUrl = process.env.AWS_QUEUE_URL;
 
-// export const sqsClient = new SQSClient({ region });
-export const sqsClient = new SQSClient({
-    region: process.env.AWS_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
-  });
+// // export const sqsClient = new SQSClient({ region });
+// export const sqsClient = new SQSClient({
+//     region: process.env.AWS_REGION,
+//     credentials: {
+//       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//     },
+//   });
